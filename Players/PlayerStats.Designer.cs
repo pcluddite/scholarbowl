09a94a4f2324fd2b98bfd48875330845bbbfe4c9
Tim Baxendale <pcluddite@hotmail.com>
2016-01-21 21:03:37 -0600

    Refactored PlayerStats form Refactored form controls to show their intent

--- Players/PlayerStats.Designer.cs
+++ Players/PlayerStats.Designer.cs
@@ -23,48 +23,48 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.playerListView = new System.Windows.Forms.ListView();
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.playerGroupBox = new System.Windows.Forms.GroupBox();
+            this.playerNameBox = new System.Windows.Forms.TextBox();
+            this.schoolGroupBox = new System.Windows.Forms.GroupBox();
+            this.teamBox = new System.Windows.Forms.ComboBox();
+            this.schoolBox = new System.Windows.Forms.ComboBox();
+            this.excelButton = new System.Windows.Forms.Button();
+            this.excelWorker = new System.ComponentModel.BackgroundWorker();
+            this.statsButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.playerGroupBox.SuspendLayout();
+            this.schoolGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listView1
+            // playerListView
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.playerListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.playerListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader3,
             this.columnHeader8,
             this.columnHeader9,
             this.columnHeader10,
             this.columnHeader11,
             this.columnHeader12});
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(12, 72);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(647, 469);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.playerListView.FullRowSelect = true;
+            this.playerListView.GridLines = true;
+            this.playerListView.Location = new System.Drawing.Point(12, 72);
+            this.playerListView.Name = "playerListView";
+            this.playerListView.Size = new System.Drawing.Size(647, 469);
+            this.playerListView.TabIndex = 0;
+            this.playerListView.UseCompatibleStateImageBehavior = false;
+            this.playerListView.View = System.Windows.Forms.View.Details;
+            this.playerListView.SelectedIndexChanged += new System.EventHandler(this.playerListView_SelectedIndexChanged);
+            this.playerListView.DoubleClick += new System.EventHandler(this.playerListView_DoubleClick);
             // 
             // columnHeader3
             // 
@@ -92,136 +92,136 @@
             // 
             this.columnHeader12.Text = "Average";
             // 
-            // groupBox1
+            // playerGroupBox
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(328, 54);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Find Player By Name";
+            this.playerGroupBox.Controls.Add(this.playerNameBox);
+            this.playerGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.playerGroupBox.Name = "playerGroupBox";
+            this.playerGroupBox.Size = new System.Drawing.Size(328, 54);
+            this.playerGroupBox.TabIndex = 1;
+            this.playerGroupBox.TabStop = false;
+            this.playerGroupBox.Text = "Find Player By Name";
             // 
-            // textBox1
+            // playerNameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(316, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.playerNameBox.Location = new System.Drawing.Point(6, 19);
+            this.playerNameBox.Name = "playerNameBox";
+            this.playerNameBox.Size = new System.Drawing.Size(316, 20);
+            this.playerNameBox.TabIndex = 2;
+            this.playerNameBox.TextChanged += new System.EventHandler(this.playerNameBox_TextChanged);
             // 
-            // groupBox2
+            // schoolGroupBox
             // 
-            this.groupBox2.Controls.Add(this.comboBox2);
-            this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Location = new System.Drawing.Point(346, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(313, 54);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Find Player By School and Team";
+            this.schoolGroupBox.Controls.Add(this.teamBox);
+            this.schoolGroupBox.Controls.Add(this.schoolBox);
+            this.schoolGroupBox.Location = new System.Drawing.Point(346, 12);
+            this.schoolGroupBox.Name = "schoolGroupBox";
+            this.schoolGroupBox.Size = new System.Drawing.Size(313, 54);
+            this.schoolGroupBox.TabIndex = 2;
+            this.schoolGroupBox.TabStop = false;
+            this.schoolGroupBox.Text = "Find Player By School and Team";
             // 
-            // comboBox2
+            // teamBox
             // 
-            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(209, 19);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(93, 21);
-            this.comboBox2.TabIndex = 1;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.teamBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.teamBox.FormattingEnabled = true;
+            this.teamBox.Location = new System.Drawing.Point(209, 19);
+            this.teamBox.Name = "teamBox";
+            this.teamBox.Size = new System.Drawing.Size(93, 21);
+            this.teamBox.TabIndex = 1;
+            this.teamBox.SelectedIndexChanged += new System.EventHandler(this.teamBox_SelectedIndexChanged);
             // 
-            // comboBox1
+            // schoolBox
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(6, 19);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(197, 21);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.schoolBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.schoolBox.FormattingEnabled = true;
+            this.schoolBox.Location = new System.Drawing.Point(6, 19);
+            this.schoolBox.Name = "schoolBox";
+            this.schoolBox.Size = new System.Drawing.Size(197, 21);
+            this.schoolBox.TabIndex = 0;
+            this.schoolBox.SelectedIndexChanged += new System.EventHandler(this.schoolBox_SelectedIndexChanged);
             // 
-            // button1
+            // excelButton
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(542, 547);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Export to Excel";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.excelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.excelButton.Location = new System.Drawing.Point(542, 547);
+            this.excelButton.Name = "excelButton";
+            this.excelButton.Size = new System.Drawing.Size(117, 23);
+            this.excelButton.TabIndex = 3;
+            this.excelButton.Text = "Export to Excel";
+            this.excelButton.UseVisualStyleBackColor = true;
+            this.excelButton.Click += new System.EventHandler(this.excelButton_Click);
             // 
-            // backgroundWorker1
+            // excelWorker
             // 
-            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
-            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            this.excelWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.excelWorker_DoWork);
+            this.excelWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.excelWorker_RunWorkerCompleted);
             // 
-            // button2
+            // statsButton
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(12, 547);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "View Player Statistics";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.listView1_DoubleClick);
+            this.statsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statsButton.Enabled = false;
+            this.statsButton.Location = new System.Drawing.Point(12, 547);
+            this.statsButton.Name = "statsButton";
+            this.statsButton.Size = new System.Drawing.Size(124, 23);
+            this.statsButton.TabIndex = 4;
+            this.statsButton.Text = "View Player Statistics";
+            this.statsButton.UseVisualStyleBackColor = true;
+            this.statsButton.Click += new System.EventHandler(this.playerListView_DoubleClick);
             // 
-            // button3
+            // removeButton
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(142, 547);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(119, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Remove Player";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.removeButton.Enabled = false;
+            this.removeButton.Location = new System.Drawing.Point(142, 547);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(119, 23);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove Player";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
             // PlayerStats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 582);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.statsButton);
+            this.Controls.Add(this.excelButton);
+            this.Controls.Add(this.schoolGroupBox);
+            this.Controls.Add(this.playerGroupBox);
+            this.Controls.Add(this.playerListView);
             this.Name = "PlayerStats";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Player Rankings";
             this.Load += new System.EventHandler(this.PlayerStats_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
+            this.playerGroupBox.ResumeLayout(false);
+            this.playerGroupBox.PerformLayout();
+            this.schoolGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView playerListView;
+        private System.Windows.Forms.GroupBox playerGroupBox;
+        private System.Windows.Forms.TextBox playerNameBox;
+        private System.Windows.Forms.GroupBox schoolGroupBox;
+        private System.Windows.Forms.ComboBox teamBox;
+        private System.Windows.Forms.ComboBox schoolBox;
+        private System.Windows.Forms.Button excelButton;
+        private System.ComponentModel.BackgroundWorker excelWorker;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader8;
         private System.Windows.Forms.ColumnHeader columnHeader9;
         private System.Windows.Forms.ColumnHeader columnHeader10;
         private System.Windows.Forms.ColumnHeader columnHeader11;
         private System.Windows.Forms.ColumnHeader columnHeader12;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button statsButton;
+        private System.Windows.Forms.Button removeButton;
     }
 }
--- Players/PlayerStats.cs
+++ Players/PlayerStats.cs
@@ -6,22 +6,26 @@ using System.Windows.Forms;
 
 namespace Scholar_Bowl
 {
-    public partial class PlayerStats : Form {
+    public partial class PlayerStats : Form
+    {
         MainForm main;
         List<ListViewItem> players = new List<ListViewItem>();
-        public PlayerStats(MainForm main) {
+        public PlayerStats(MainForm main)
+        {
             this.main = main;
             InitializeComponent();
         }
 
-        private void PlayerStats_Load(object sender, EventArgs e) {
+        private void PlayerStats_Load(object sender, EventArgs e)
+        {
             reload();
-            comboBox1.Items.Add("");
-            comboBox1.Items.AddRange(MainForm.Schools.Keys.ToArray());
+            schoolBox.Items.Add("");
+            schoolBox.Items.AddRange(MainForm.Schools.Keys.ToArray());
         }
 
-        void reload() {
-            listView1.Items.Clear();
+        private void reload()
+        {
+            playerListView.Items.Clear();
             players.Clear();
 
             List<Player> rankedPlayers = MainForm.Players.Values.ToList();
@@ -46,104 +50,112 @@ namespace Scholar_Bowl
                 item.SubItems.Add(MainForm.AllMatches.GetTotalTossups(p).ToString());
                 item.SubItems.Add(games.ToString());
                 if (avg > -1) {
-                    item.SubItems.Add(avg.ToString("0.###").PadLeft(1, '0'));
+                    item.SubItems.Add(avg.ToString("0.###"));
                 }
                 else {
                     item.SubItems.Add("--");
                 }
-                listView1.Items.Add(item);
+                playerListView.Items.Add(item);
                 players.Add(item);
                 lastAvg = avg;
                 lastGames = games;
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e) {
-            listView1.Items.Clear();
+        private void playerNameBox_TextChanged(object sender, EventArgs e)
+        {
+            playerListView.Items.Clear();
             foreach (ListViewItem p in players) {
-                if (p.SubItems[1].Text.ToLower().Contains(textBox1.Text.ToLower())) {
-                    listView1.Items.Add(p);
+                if (p.SubItems[1].Text.ToLower().Contains(playerNameBox.Text.ToLower())) {
+                    playerListView.Items.Add(p);
                 }
             }
-            comboBox1.SelectedIndex = comboBox2.SelectedIndex = -1;
+            schoolBox.SelectedIndex = teamBox.SelectedIndex = -1;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
-            comboBox2.Items.Clear();
-            if (comboBox1.SelectedIndex > 0) {
-                textBox1.Text = "";
-                comboBox2.Items.Add("");
-                comboBox2.Items.AddRange(MainForm.Schools[comboBox1.SelectedItem.ToString()].Teams.Keys.ToArray());
+        private void schoolBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            teamBox.Items.Clear();
+            if (schoolBox.SelectedIndex > 0) {
+                playerNameBox.Text = "";
+                teamBox.Items.Add("");
+                teamBox.Items.AddRange(MainForm.Schools[schoolBox.SelectedItem.ToString()].Teams.Keys.ToArray());
             }
-            listView1.Items.Clear();
+            playerListView.Items.Clear();
             foreach (ListViewItem p in players) {
-                if (p.SubItems[2].Text.StartsWith(comboBox1.SelectedItem.ToString())) {
-                    listView1.Items.Add(p);
+                if (p.SubItems[2].Text.StartsWith(schoolBox.SelectedItem.ToString())) {
+                    playerListView.Items.Add(p);
                 }
             }
         }
 
-        void listViewAdd(string[] p) {
+        private void listViewAdd(string[] p)
+        {
             ListViewItem item = new ListViewItem(p[0]);
             item.SubItems.Add(p[1]);
             item.SubItems.Add(p[2]);
             item.SubItems.Add(p[3]);
             item.SubItems.Add(p[4]);
             item.SubItems.Add(p[5]);
-            listView1.Items.Add(item);
+            playerListView.Items.Add(item);
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) {
-            if (comboBox2.SelectedIndex < 0) {
+        private void teamBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (teamBox.SelectedIndex < 0) {
                 return;
             }
-            listView1.Items.Clear();
+            playerListView.Items.Clear();
             foreach (ListViewItem p in players) {
-                if (p.SubItems[2].Text.StartsWith(comboBox1.SelectedItem.ToString()) && 
-                    p.SubItems[2].Text.EndsWith(comboBox2.SelectedItem.ToString())) {
-                        listView1.Items.Add(p);
+                if (p.SubItems[2].Text.StartsWith(schoolBox.SelectedItem.ToString()) &&
+                    p.SubItems[2].Text.EndsWith(teamBox.SelectedItem.ToString())) {
+                    playerListView.Items.Add(p);
                 }
             }
         }
 
-        private void listView1_DoubleClick(object sender, EventArgs e) {
-            if (listView1.SelectedItems.Count == 1) {
-                ViewPlayer vp = new ViewPlayer(MainForm.Players[listView1.SelectedItems[0].SubItems[1].Text]);
+        private void playerListView_DoubleClick(object sender, EventArgs e)
+        {
+            if (playerListView.SelectedItems.Count == 1) {
+                ViewPlayer vp = new ViewPlayer(MainForm.Players[playerListView.SelectedItems[0].SubItems[1].Text]);
                 vp.ShowDialog(this);
                 reload();
             }
         }
 
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
-            List<Player> players = null;
-            Invoke(new MethodInvoker(delegate { players = MainForm.Players.Values.ToList(); }));
-            ExcelGenerator.AllPlayers(players);
+        private void excelWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ExcelGenerator.AllPlayers((List<Player>)e.Argument);
         }
 
-        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
-            groupBox1.Enabled = groupBox2.Enabled =
-                listView1.Enabled = button1.Enabled = true;
+        private void excelWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            playerGroupBox.Enabled = schoolGroupBox.Enabled =
+                playerListView.Enabled = excelButton.Enabled = true;
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            groupBox1.Enabled = groupBox2.Enabled =
-                listView1.Enabled = button1.Enabled = false;
-            backgroundWorker1.RunWorkerAsync();
+        private void excelButton_Click(object sender, EventArgs e)
+        {
+            playerGroupBox.Enabled = schoolGroupBox.Enabled =
+                playerListView.Enabled = excelButton.Enabled = false;
+            excelWorker.RunWorkerAsync(MainForm.Players.Values.ToList());
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
-            button2.Enabled = 
-                button3.Enabled = (listView1.SelectedItems.Count == 1);
+        private void playerListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            statsButton.Enabled =
+                removeButton.Enabled = (playerListView.SelectedItems.Count == 1);
         }
 
-        private void button3_Click(object sender, EventArgs e) {
-            if (!listView1.SelectedItems[0].SubItems[4].Text.Equals("0")) {
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (!playerListView.SelectedItems[0].SubItems[4].Text.Equals("0")) {
                 MessageBox.Show(this, "This player has already participated in some games and cannot be removed from the roster.",
                     this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            else if (MessageBox.Show(this, "Are you sure you want to remove this player from the roster?", this.Text,
-             MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes) {
-                MainForm.Players.Remove(listView1.SelectedItems[0].SubItems[1].Text);
+            else if (MessageBox.Show(this, "Are you sure you want to remove this player from the roster?", Text,
+             MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
+                MainForm.Players.Remove(playerListView.SelectedItems[0].SubItems[1].Text);
                 reload();
             }
         }
